801dd66b90db673e46ee50e1a03d56a4f0d12475
bubble <bubble@caradhras.lan>
2014-03-31 23:05:21 +0200

    ndk 9 fetch

--- gnustep-android/gnustep-android/phases/11-ndk-environment-macosx.sh
+++ gnustep-android/gnustep-android/phases/11-ndk-environment-macosx.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-cd build
-echo -n "Downloading NDK r9d..."
-wget http://dl.google.com/android/ndk/android-ndk-r9d-darwin-x86.tar.bz2
-echo "Done."
-cd ..
-export ANDROID_NDK="${ANDROID_GNUSTEP_INSTALL_ROOT}/android-ndk-r8d"
-export ANDROID_NDK_LLVM="${ANDROID_GNUSTEP_INSTALL_ROOT}/android-ndk-r8d/toolchains/llvm-3.1/prebuilt/linux-x86/bin/"
+export ANDROID_NDK="${ANDROID_GNUSTEP_INSTALL_ROOT}/android-ndk-r9d"
+export ANDROID_NDK_LLVM="${ANDROID_GNUSTEP_INSTALL_ROOT}/android-ndk-r9d/toolchains/llvm-3.1/prebuilt/linux-x86/bin/"
